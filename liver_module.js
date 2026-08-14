432c4d2225fd7496e424d14c8f99da8500a1d4c3
lvwmwm <luizrossi17@gmail.com>
2026-08-14 13:09:06 -0300

    isrc resolve stuff

--- liver_module.js
+++ liver_module.js
@@ -15,6 +15,73 @@ var CFG = {
 var UA = { "Accept": "application/json" };
 var ISRC_RE = /^[A-Z]{2}[A-Z0-9]{3}[0-9]{7}$/i;
 
+function md5(str) {
+  function rl(n, c) { return (n << c) | (n >>> (32 - c)); }
+  function au(x, y) {
+    var l = (x & 0xFFFF) + (y & 0xFFFF);
+    var m = (x >> 16) + (y >> 16) + (l >> 16);
+    return (m << 16) | (l & 0xFFFF);
+  }
+  function cmn(q, a, b, x, s, t) { return au(rl(au(au(a, q), au(x, t)), s), b); }
+  function ff(a, b, c, d, x, s, t) { return cmn((b & c) | ((~b) & d), a, b, x, s, t); }
+  function gg(a, b, c, d, x, s, t) { return cmn((b & d) | (c & (~d)), a, b, x, s, t); }
+  function hh(a, b, c, d, x, s, t) { return cmn(b ^ c ^ d, a, b, x, s, t); }
+  function ii(a, b, c, d, x, s, t) { return cmn(c ^ (b | (~d)), a, b, x, s, t); }
+  function toBlocks(s) {
+    var n = s.length, nblk = ((n + 8) >> 6) + 1, blks = [], i;
+    for (i = 0; i < nblk * 16; i++) blks[i] = 0;
+    for (i = 0; i < n; i++) blks[i >> 2] |= s.charCodeAt(i) << ((i % 4) * 8);
+    blks[i >> 2] |= 0x80 << ((i % 4) * 8);
+    blks[nblk * 16 - 2] = n * 8;
+    return blks;
+  }
+  function toHex(num) {
+    var s = "", j;
+    for (j = 0; j < 4; j++) s += ("0" + ((num >> (j * 8)) & 0xFF).toString(16)).slice(-2);
+    return s;
+  }
+  var input = unescape(encodeURIComponent(String(str)));
+  var x = toBlocks(input);
+  var a = 1732584193, b = -271733879, c = -1732584194, d = 271733878, i;
+  for (i = 0; i < x.length; i += 16) {
+    var oa = a, ob = b, oc = c, od = d;
+    a = ff(a, b, c, d, x[i], 7, -680876936); d = ff(d, a, b, c, x[i + 1], 12, -389564586);
+    c = ff(c, d, a, b, x[i + 2], 17, 606105819); b = ff(b, c, d, a, x[i + 3], 22, -1044525330);
+    a = ff(a, b, c, d, x[i + 4], 7, -176418897); d = ff(d, a, b, c, x[i + 5], 12, 1200080426);
+    c = ff(c, d, a, b, x[i + 6], 17, -1473231341); b = ff(b, c, d, a, x[i + 7], 22, -45705983);
+    a = ff(a, b, c, d, x[i + 8], 7, 1770035416); d = ff(d, a, b, c, x[i + 9], 12, -1958414417);
+    c = ff(c, d, a, b, x[i + 10], 17, -42063); b = ff(b, c, d, a, x[i + 11], 22, -1990404162);
+    a = ff(a, b, c, d, x[i + 12], 7, 1804603682); d = ff(d, a, b, c, x[i + 13], 12, -40341101);
+    c = ff(c, d, a, b, x[i + 14], 17, -1502002290); b = ff(b, c, d, a, x[i + 15], 22, 1236535329);
+    a = gg(a, b, c, d, x[i + 1], 5, -165796510); d = gg(d, a, b, c, x[i + 6], 9, -1069501632);
+    c = gg(c, d, a, b, x[i + 11], 14, 643717713); b = gg(b, c, d, a, x[i], 20, -373897302);
+    a = gg(a, b, c, d, x[i + 5], 5, -701558691); d = gg(d, a, b, c, x[i + 10], 9, 38016083);
+    c = gg(c, d, a, b, x[i + 15], 14, -660478335); b = gg(b, c, d, a, x[i + 4], 20, -405537848);
+    a = gg(a, b, c, d, x[i + 9], 5, 568446438); d = gg(d, a, b, c, x[i + 14], 9, -1019803690);
+    c = gg(c, d, a, b, x[i + 3], 14, -187363961); b = gg(b, c, d, a, x[i + 8], 20, 1163531501);
+    a = gg(a, b, c, d, x[i + 13], 5, -1444681467); d = gg(d, a, b, c, x[i + 2], 9, -51403784);
+    c = gg(c, d, a, b, x[i + 7], 14, 1735328473); b = gg(b, c, d, a, x[i + 12], 20, -1926607734);
+    a = hh(a, b, c, d, x[i + 5], 4, -378558); d = hh(d, a, b, c, x[i + 8], 11, -2022574463);
+    c = hh(c, d, a, b, x[i + 11], 16, 1839030562); b = hh(b, c, d, a, x[i + 14], 23, -35309556);
+    a = hh(a, b, c, d, x[i + 1], 4, -1530992060); d = hh(d, a, b, c, x[i + 4], 11, 1272893353);
+    c = hh(c, d, a, b, x[i + 7], 16, -155497632); b = hh(b, c, d, a, x[i + 10], 23, -1094730640);
+    a = hh(a, b, c, d, x[i + 13], 4, 681279174); d = hh(d, a, b, c, x[i], 11, -358537222);
+    c = hh(c, d, a, b, x[i + 3], 16, -722521979); b = hh(b, c, d, a, x[i + 6], 23, 76029189);
+    a = hh(a, b, c, d, x[i + 9], 4, -640364487); d = hh(d, a, b, c, x[i + 12], 11, -421815835);
+    c = hh(c, d, a, b, x[i + 15], 16, 530742520); b = hh(b, c, d, a, x[i + 2], 23, -995338651);
+    a = ii(a, b, c, d, x[i], 6, -198630844); d = ii(d, a, b, c, x[i + 7], 10, 1126891415);
+    c = ii(c, d, a, b, x[i + 14], 15, -1416354905); b = ii(b, c, d, a, x[i + 5], 21, -57434055);
+    a = ii(a, b, c, d, x[i + 12], 6, 1700485571); d = ii(d, a, b, c, x[i + 3], 10, -1894986606);
+    c = ii(c, d, a, b, x[i + 10], 15, -1051523); b = ii(b, c, d, a, x[i + 1], 21, -2054922799);
+    a = ii(a, b, c, d, x[i + 8], 6, 1873313359); d = ii(d, a, b, c, x[i + 15], 10, -30611744);
+    c = ii(c, d, a, b, x[i + 6], 15, -1560198380); b = ii(b, c, d, a, x[i + 13], 21, 1309151649);
+    a = ii(a, b, c, d, x[i + 4], 6, -145523070); d = ii(d, a, b, c, x[i + 11], 10, -1120210379);
+    c = ii(c, d, a, b, x[i + 2], 15, 718787259); b = ii(b, c, d, a, x[i + 9], 21, -343485551);
+    a = au(a, oa); b = au(b, ob); c = au(c, oc); d = au(d, od);
+  }
+  return toHex(a) + toHex(b) + toHex(c) + toHex(d);
+}
+
 function delay(ms) { return new Promise(function (r) { setTimeout(r, ms); }); }
 function okJson(res) {
   if (!res || !res.ok) throw new Error((res && res.status) ? ("http " + res.status) : "net fail");
@@ -171,7 +238,6 @@ function exactMatchOrInverse(item, parts) {
   return false;
 }
 function nameOf(a) { return typeof a === "string" ? a : ((a && a.name) || ""); }
-
 var geoCache = null, geoFetching = null, geoNextTry = 0;
 function loadGeo(force) {
   if (geoCache && Date.now() - geoCache.ts < 43200000 && !force) return Promise.resolve(geoCache);
@@ -191,22 +257,7 @@ function loadGeo(force) {
     if (!g.base) g.base = "https://www.qobuz.com/api.json/0.2";
     if (!g.tidalapi) g.tidalapi = "https://tidal-come-one.francescone.workers.dev";
     if (!g.tidalsearch) g.tidalsearch = "https://lol.samidy.workers.dev";
-    var m5 = src.match(/function\s+md5\s*\([^)]*\)\s*\{/);
-    if (m5) {
-      var open = src.indexOf("{", m5.index);
-      var depth = 0, end = -1;
-      for (var i = open; i < src.length; i++) {
-        if (src[i] === "{") depth++;
-        else if (src[i] === "}") { depth--; if (!depth) { end = i; break; } }
-      }
-      if (end > open) {
-        try {
-          var f = new Function("return (" + src.slice(m5.index, end + 1) + ")");
-          var fn = f();
-          if (typeof fn === "function" && fn("abc") === "900150983cd24fb0d6963f7d28e17f72") g.md5 = fn;
-        } catch (e) { g.md5 = null; }
-      }
-    }
+    g.md5 = md5;
     geoCache = g;
     geoFetching = null;
     return g;
@@ -385,13 +436,20 @@ function tidalByIsrc(isrc, q) {
     });
   });
 }
-function resolveIsrc(isrc, q) {
-  if (!ISRC_RE.test(String(isrc || ""))) return Promise.reject(new Error("bad isrc (not ISRC format)"));
-  return qbzByIsrc(isrc, q).then(null, function () {
-    return tidalByIsrc(isrc, q);
+function deezerByIsrc(isrc, q) {
+  return throttled("dzr", CFG.deezerPub + "/track/isrc:" + encodeURIComponent(isrc)).then(okJson).then(function (d) {
+    if (!d || d.error || d.id == null) throw new Error("dzr: isrc miss");
+    return octStream(String(d.id), [q], false);
   });
 }
-
+function resolveIsrc(isrc, q) {
+  if (!ISRC_RE.test(String(isrc || ""))) return Promise.reject(new Error("bad isrc (not ISRC format)"));
+  return deezerByIsrc(isrc, q).then(null, function () {
+    return qbzByIsrc(isrc, q).then(null, function () {
+      return tidalByIsrc(isrc, q);
+    });
+  });
+}
 var octTokens = {}, scache = {};
 function mapDeezerTrack(t, lbl) {
   var album = t.album || {};
@@ -508,7 +566,6 @@ function searchTracks(query, limit, ctx) {
     function () { return tidalSearchLane(query, n); }
   ]);
 }
-
 function getAlbum(id) {
   var p = parseTrackId(String(id || ""));
   var deezerLike = function (d, prefix) {
@@ -609,7 +666,7 @@ return {
   id: "liver",
   name: "Liver",
   author: "Livie",
-  version: "3.1.0",
+  version: "3.1.1",
   description: "best module in the universe",
   labels: ["MP3", "FLAC", "LOSSLESS", "MULTI-SOURCE"],
 
